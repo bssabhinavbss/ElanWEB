2fadc29e53075675dcb2f0fe25997933f0f372d8
bssabhinavbss <cs24btech11014@iith.ac.in>
2025-05-26 20:31:48 +0530

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,1 +1,27 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import Hero from './components/Hero';
+import Theme from './components/Theme';
+import Merchandise from './components/Merchandise';
+import Aftermovies from './components/Aftermovies';
+import Gallery from './components/Gallery';
+import GuestCards from './components/GuestCards';
+import About from './components/About';
+import './index.css';
 
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <Theme />
+      <Merchandise />
+      <Aftermovies />
+      <Gallery />
+      <GuestCards />
+      <About/>
+    </div>
+  );
+};
+
+export default App;
