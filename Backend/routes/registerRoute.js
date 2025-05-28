75db1b7ce9faf300c10a8400b889aab23d6cc2e4
bssabhinavbss <cs24btech11014@iith.ac.in>
2025-05-28 10:17:22 +0530

    Update registerRoute.js

--- Backend/routes/registerRoute.js
+++ Backend/routes/registerRoute.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { registerUser } = require("../controllers/registerController");
 
-router.post("/register", registerUser); // <-- Add '/register' here
+router.post("/", registerUser); // <-- Add '/register' here
 
 module.exports = router;
